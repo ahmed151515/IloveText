ef17805bd0091eb40b2ff7a128e3b6f03b19dbac
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-11-02 19:27:43 +0200

    .

--- src/forms.py
+++ src/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
+row = 10
+col = 100
 
 
 class SummarizeForm(FlaskForm):
-    text = TextAreaField('Text')
+    text = TextAreaField('Text', render_kw={"rows": row, "cols": col})
     submit = SubmitField('Summarize')
-    result = TextAreaField('Result', render_kw={'readonly': True})
+    result = TextAreaField('Result', render_kw={
+                           'readonly': True, "rows": row, "cols": col})
