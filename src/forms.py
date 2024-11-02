93565bf53209bd2fe0daaae064019d017b4b9969
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-11-02 19:18:36 +0200

    translation making baby steps

--- src/forms.py
+++ src/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, StringField
+from wtforms.validators import DataRequired
 
 
 class SummarizeForm(FlaskForm):
     text = TextAreaField('Text')
     submit = SubmitField('Summarize')
     result = TextAreaField('Result', render_kw={'readonly': True})
+
+class InputForm(FlaskForm):
+    """
+    """
+    input = StringField("input", validators=[DataRequired()])
+    submit = SubmitField('translate')
+    result = TextAreaField('output')
