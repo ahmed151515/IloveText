c556eac209f9c9545c049c3bdcddf663301f3588
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-11-02 19:32:28 +0200

    .

--- src/forms.py
+++ src/forms.py
@@ -1,18 +1,18 @@
-from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
 from wtforms import TextAreaField, SubmitField, StringField
+
+
 row = 10
 col = 100
 
 
 class SummarizeForm(FlaskForm):
-    text = TextAreaField('Text', render_kw={"rows": row, "cols": col})
+    text = TextAreaField('Text', render_kw={"rows": row, "cols": col}, validators=[
+                         DataRequired()])
     submit = SubmitField('Summarize')
-
-
-result = TextAreaField('Result', render_kw={
-    'readonly': True, "rows": row, "cols": col})
-result = TextAreaField('Result', render_kw={'readonly': True})
+    result = TextAreaField('Result', render_kw={
+                           'readonly': True, "rows": row, "cols": col})
 
 
 class InputForm(FlaskForm):
@@ -21,6 +21,3 @@ class InputForm(FlaskForm):
     input = StringField("input", validators=[DataRequired()])
     submit = SubmitField('translate')
     result = TextAreaField('output')
-
-
->>>>>> > 93565bf53209bd2fe0daaae064019d017b4b9969
